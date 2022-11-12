b86aadef3b89c62f20d603d1e9b957b1b58adf8b
squoop <the.sam.mcg@protonmail.com>
2022-11-12 17:15:27 +0000

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use anyhow::{bail, Context, Result};
+
+fn main() -> Result<()> {
+    let args: Vec<String> = std::env::args().skip(1).collect();
+
+    match args.as_slice() {
+        [path] => {
+            let source = std::fs::read_to_string(path).context("couldn't read input file")?;
+            run_source(&source);
+        }
+        _ => bail!("expected one argument `path`"),
+    }
+
+    Ok(())
+}
+
+fn run_source(source: &str) {
+    println!("{source}");
+}
