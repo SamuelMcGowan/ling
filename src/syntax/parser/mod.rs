f5056a01f674dd0a6965b3fc1c2a87706cfd2f15
squoop <the.sam.mcg@protonmail.com>
2022-11-21 20:29:24 +0000

    Basic parser methods.

---
+++ src/syntax/parser/mod.rs
@@ -0,0 +1,50 @@
+use std::iter::Peekable;
+use std::vec::IntoIter;
+
+use super::token::{Token, TokenKind};
+use super::ParseContext;
+
+impl<'a> ParseContext<'a> {
+    pub fn parser(&mut self) -> Parser<'_, 'a> {
+        // Clippy thinks this is avoidable.
+        // It probably is, but it's a lot of work to avoid one allocation.
+        #[allow(clippy::needless_collect)]
+        let tokens: Vec<_> = self.lexer().collect();
+
+        Parser {
+            tokens: tokens.into_iter().peekable(),
+            context: self,
+        }
+    }
+}
+
+pub(crate) struct Parser<'ctx, 'a> {
+    tokens: Peekable<IntoIter<Token>>,
+    context: &'ctx ParseContext<'a>,
+}
+
+impl Parser<'_, '_> {
+    fn next(&mut self) -> Option<Token> {
+        self.tokens.next()
+    }
+
+    fn peek(&mut self) -> Option<Token> {
+        self.tokens.peek().copied()
+    }
+
+    fn eat(&mut self, kind: TokenKind) -> bool {
+        match self.peek() {
+            Some(token) if token.kind == kind => {
+                self.tokens.next();
+                true
+            }
+            _ => false,
+        }
+    }
+
+    fn eat_while(&mut self, mut predicate: impl FnMut(TokenKind) -> bool) {
+        while matches!(self.peek(), Some(token) if predicate(token.kind)) {
+            self.next();
+        }
+    }
+}
